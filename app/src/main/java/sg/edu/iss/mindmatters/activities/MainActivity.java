6a0fc04f16ac4305f9c047c0e12700efad047e75
Juswang <64295744+Juswang@users.noreply.github.com>
2021-02-08 17:00:44 +0800

    merged with lm's side. added non logged in user functionality

--- app/src/main/java/sg/edu/iss/mindmatters/activities/MainActivity.java
+++ app/src/main/java/sg/edu/iss/mindmatters/activities/MainActivity.java
@@ -7,8 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import sg.edu.iss.mindmatters.MyApplication;
@@ -18,11 +27,49 @@ import sg.edu.iss.mindmatters.R;
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     SharedPreferences pref;
+    private LineChart linechart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linechart=(LineChart)findViewById(R.id.linechart);
+        linechart.setDragEnabled(true);
+        linechart.setScaleEnabled(false);
+        ArrayList<Entry>yValues=new ArrayList<>();
+        ArrayList<Entry>xValues=new ArrayList<>();
+        yValues.add(new Entry(1,50f));
+        yValues.add(new Entry(2,80f));
+        yValues.add(new Entry(3,50f));
+        yValues.add(new Entry(4,30f));
+        yValues.add(new Entry(5,20f));
+        yValues.add(new Entry(6,107f));
+        yValues.add(new Entry(7,80f));
+        xValues.add(new Entry(1,30f));
+        xValues.add(new Entry(2,25f));
+        xValues.add(new Entry(3,40f));
+        xValues.add(new Entry(4,33f));
+        xValues.add(new Entry(5,99f));
+        xValues.add(new Entry(6,10f));
+        xValues.add(new Entry(7,57f));
+
+        LineDataSet set2=new LineDataSet(xValues,"dataSet2");
+        LineDataSet set1=new LineDataSet(yValues,"dataSet1");
+        set1.setFillAlpha(110);
+        set1.setFillColor(android.R.color.black);
+        set1.setLineWidth(3f);
+        set1.setDrawFilled(true);
+        set2.setFillAlpha(85);
+        set2.setFillColor(android.R.color.black);
+        set2.setLineWidth(3f);
+        set2.setDrawFilled(true);
+        ArrayList<ILineDataSet>datasets=new ArrayList<>();
+        datasets.add(set1);
+        datasets.add(set2);
+
+        LineData data = new LineData(datasets);
+        linechart.setData(data);
 
         pref = getSharedPreferences(
                 "user_credentials", MODE_PRIVATE);
